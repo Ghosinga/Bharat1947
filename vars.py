5f2ac2474952b49ac77c6eb72987ac051c67b61e
Ghosinga <148040605+Ghosinga@users.noreply.github.com>
2025-06-13 08:19:00 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 # DON'T add anything here just add in render's secret or env section 
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "27612149"))
+API_HASH = environ.get("API_HASH", "9843377992b0b1fbc61d80be068c8d94")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
